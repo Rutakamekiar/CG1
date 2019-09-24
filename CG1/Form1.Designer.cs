1bf943d17db9f18648c376473a7272d63afc8bea
v.kovaliov <v.kovaliov@polytech.software>
2019-09-24 15:08:24 +0300

    Lab2 part 2

--- CG1/Form1.Designer.cs
+++ CG1/Form1.Designer.cs
@@ -29,6 +29,9 @@
         private void InitializeComponent()
         {
             this.Picture = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.Lab1 = new System.Windows.Forms.Button();
+            this.Lab2 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.Picture)).BeginInit();
             this.SuspendLayout();
             // 
@@ -42,11 +45,44 @@
             this.Picture.TabIndex = 0;
             this.Picture.TabStop = false;
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(2, 714);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(136, 35);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.RunButton);
+            // 
+            // Lab1
+            // 
+            this.Lab1.Location = new System.Drawing.Point(206, 714);
+            this.Lab1.Name = "Lab1";
+            this.Lab1.Size = new System.Drawing.Size(140, 35);
+            this.Lab1.TabIndex = 2;
+            this.Lab1.Text = "Lab1";
+            this.Lab1.UseVisualStyleBackColor = true;
+            this.Lab1.Click += new System.EventHandler(this.Lab1_Click);
+            // 
+            // Lab2
+            // 
+            this.Lab2.Location = new System.Drawing.Point(445, 714);
+            this.Lab2.Name = "Lab2";
+            this.Lab2.Size = new System.Drawing.Size(123, 35);
+            this.Lab2.TabIndex = 3;
+            this.Lab2.Text = "Lab2";
+            this.Lab2.UseVisualStyleBackColor = true;
+            this.Lab2.Click += new System.EventHandler(this.Lab2_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1600, 1181);
+            this.Controls.Add(this.Lab2);
+            this.Controls.Add(this.Lab1);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.Picture);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -58,6 +94,9 @@
         #endregion
 
         private System.Windows.Forms.PictureBox Picture;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button Lab1;
+        private System.Windows.Forms.Button Lab2;
     }
 }
 
--- CG1/Form1.cs
+++ CG1/Form1.cs
@@ -6,50 +6,134 @@ namespace CG1
 {
     public partial class Form1 : Form
     {
+        private static Graphics _graph;
+        private static Bitmap bmp;
         public Form1()
         {
             InitializeComponent();
-            ////DrawLab1();
-            DrawLab2();
 
+            bmp = new Bitmap(Picture.Width, Picture.Height);
+            _graph = Graphics.FromImage(bmp);
+            _graph.Clear(Color.White);
+            Picture.Image = bmp;
+            DrawLab2();
         }
 
-        private void DrawLab2(int n = 8, int r = 100)
+        private void DrawLab2(int count = 100, int n = 8, int r = 300)
         {
-            var bmp = new Bitmap(Picture.Width, Picture.Height);
-            var graph = Graphics.FromImage(bmp);
+
             var pen = new Pen(Color.Red);
             var pen2 = new Pen(Color.CornflowerBlue);
-            var center = new Point(ClientSize.Width / 2,ClientSize.Height/2);
-            graph.DrawRectangle(pen, new Rectangle(center, new Size(20,20)));
-            graph.DrawFigureLab2(pen, center, r, n, 40);
-            graph.DrawFigureLab2(pen2, center, r, n);
+            var center = new Point(400, 350);
 
-            Picture.Image = bmp;
-        }
+            _graph.TranslateTransform(center.X, center.Y);
+            _graph.DrawFigureLab2(pen, r, n);
+            _graph.RotateTransform(26);
+            _graph.DrawFigureLab2(pen2, r, n);
+            _graph.RotateTransform(-26);
 
-        private void DrawLab1(int count = 10)
-        {
-            var bmp = new Bitmap(Picture.Width, Picture.Height);
-            var graph = Graphics.FromImage(bmp);
-            var rectangle = new Rectangle(new Point(500, 500), new Size(700, 700));
-            graph.DrawRectangle(new Pen(Color.Red), rectangle);
-            graph.DrawFigureLab1(Extensions.DrawByCenter(rectangle));
-            var fi = 360 / count;
-
-            var r = rectangle.Size.Width / 2;
-            for (var i = 0; i < count; i++)
+            if (count != 0)
             {
-                var x = (int) (r * Math.Cos(i * fi) / 2 + rectangle.Location.X);
-                var y = (int) (r * Math.Sin(i * fi) / 2 + rectangle.Location.X);
-                var rectangle2 = new Rectangle(new Point(x, y),
-                    new Size(rectangle.Size.Width / 2, rectangle.Size.Height / 2));
-                graph.DrawFigureLab1(Extensions.DrawByCenter(rectangle2));
-            }
+                var fi = 360 / count;
 
+                r /= 2;
+                _graph.DrawOrnamentLab2(pen, count, r, fi, n);
+
+                _graph.RotateTransform(26);
+                _graph.DrawOrnamentLab2(pen2, count, r, fi, n);
+                _graph.RotateTransform(-26);
+            }
+        }
+
+        private void DrawLab1(int count = 100)
+        {
+            var rectangle = new Rectangle(new Point(350, 350), new Size(700, 700));
+            _graph.DrawFigureLab1(Extensions.DrawByCenter(rectangle));
+            if (count !=0 )
+            {
+                var fi = 360 / count;
+
+                var r = rectangle.Size.Width / 2;
+                for (var i = 0; i < count; i++)
+                {
+                    var x = (int)(r * Math.Cos(i * fi) / 2 + rectangle.Location.X);
+                    var y = (int)(r * Math.Sin(i * fi) / 2 + rectangle.Location.X);
+                    var rectangle2 = new Rectangle(new Point(x, y),
+                                                   new Size(rectangle.Size.Width / 2, rectangle.Size.Height / 2));
+                    _graph.DrawFigureLab1(Extensions.DrawByCenter(rectangle2));
+                }
+            }
+        }
+
+        private void RunButton(object sender, EventArgs e)
+        {
+            var timer = new Timer { Interval = 30 };
+            var count = 0;
+            var min = 0;
+            var max = 180;
+            var isRight = true;
+            var point = 10;
+            _graph.Clear(Color.Black);
+            DrawLab1();
+            _graph.TranslateTransform(point, 0);
+            Picture.Image = bmp;
+            var value = 1;
+            _graph.ResetTransform();
+
+            timer.Tick += (o, ev) =>
+            {
+                if (isRight)
+                    point += 5;
+                else point -= 5;
+                
+                _graph.TranslateTransform(point, 0);
+                _graph.Clear(Color.Black);
+                DrawLab1();
+                Picture.Image = bmp;
+                _graph.ResetTransform();
+
+                count += value;
+                if (count > max)
+                {
+                    isRight = false;
+                    value = -1;
+                }
+                else if (count < min)
+                {
+                    isRight = true;
+                    value = 1;
+                }
+
+                //if (count > max)
+                //{
+                //    var t = o as Timer;
+                //    t.Stop();
+                //}
+            };
+            timer.Start();
+        }
+
+        private void Lab1_Click(object sender, EventArgs e)
+        {
+            _graph.ResetTransform();
+            _graph.Clear(Color.White);
+            DrawLab1(100);
+            _graph.TranslateTransform(800,0);
+            DrawLab1(10);
             Picture.Image = bmp;
         }
 
+        private void Lab2_Click(object sender, EventArgs e)
+        {
+            _graph.ResetTransform();
+            _graph.Clear(Color.Black);
+            DrawLab2(100);
+            _graph.ResetTransform();
+
+            _graph.TranslateTransform(800, 0);
+            DrawLab2(10);
+            Picture.Image = bmp;
+        }
     }
 
     public static class Extensions
@@ -64,6 +148,7 @@ namespace CG1
         public static void DrawFigureLab1(this Graphics graph, Rectangle rect)
         {
             var pen = new Pen(Color.Blue);
+            var pen2 = new Pen(Color.Red);
             graph.DrawEllipse(pen, rect);
 
             var rectSizeW = (int) (rect.Size.Width / Math.Sqrt(2));
@@ -72,24 +157,30 @@ namespace CG1
             var rectLocationY = (rect.Height - rectSizeH) / 2 + rect.Location.Y;
             rect.Size = new Size(rectSizeW, rectSizeH);
             rect.Location = new Point(rectLocationX, rectLocationY);
-            graph.DrawRectangle(pen, rect);
+            graph.DrawRectangle(pen2, rect);
         }
 
-        public static void DrawFigureLab2(this Graphics graph, Pen pen, Point center, int r = 100, int n = 10, int angle = 0)
+        public static void DrawFigureLab2(this Graphics graph, Pen pen, int r = 100, int n = 10)
         {
-            var angleRadian = angle * Math.PI / 180;
             for (var t = 0d; t < n * Math.PI; t += 0.01)
             {
                 var x = r * (Math.Cos(t) - Math.Cos(n * t) / n);
                 var y = r * (Math.Sin(t) - Math.Sin(n * t) / n);
-                x = x * Math.Cos(angleRadian) - y * Math.Sin(angleRadian);
-                y = x * Math.Sin(angleRadian) + y * Math.Cos(angleRadian);
-
-                var rectangle = new Rectangle(new Point((int) x + center.X, (int) y + center.Y), new Size(1, 1));
-                
+                var rectangle = new Rectangle(new Point((int) x, (int) y), new Size(1, 1));
                 graph.DrawEllipse(pen, rectangle);
             }
-            graph.ResetTransform();
+        }
+
+        public static void DrawOrnamentLab2(this Graphics graph, Pen pen, int count, int r, int fi, int n)
+        {
+            for (var i = 0; i < count; i++)
+            {
+                var x = (int)(r * Math.Cos(i * fi));
+                var y = (int)(r * Math.Sin(i * fi));
+                graph.TranslateTransform(x, y);
+                graph.DrawFigureLab2(pen, r, n);
+                graph.TranslateTransform(-x, -y);
+            }
         }
     }
 }
